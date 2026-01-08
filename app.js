42e682dd43e41f9f4bc90db3e7cbbf181ff0c62f
erenizgi <erenizgi001@gmail.com>
2026-01-09 01:52:10 +0300

    sövdüm ve tekrar yazdım

--- app.js
+++ app.js
@@ -1,64 +1,63 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+const fs = require('fs');
+
+let win;
+
+const userDataPath = path.join(app.getPath('userData'), 'music_youtube_data');
+
+if (!fs.existsSync(userDataPath)) {
+  fs.mkdirSync(userDataPath, { recursive: true });
+}
+app.setPath('userData', userDataPath);
+
+// app.disableHardwareAcceleration();
 
 function createWindow() {
-  Menu.setApplicationMenu(null);
-  app.setName("youtube-music-desktop-app");
-  app.commandLine.appendSwitch('wm-class', 'youtube-music');
-  const win = new BrowserWindow({
+  app.setName('youtube-music-desktop-app');
+  app.commandLine.appendSwitch('wm-class', 'youtube-music-desktop-app');
+
+  win = new BrowserWindow({
     width: 1200,
     height: 800,
     title: 'YouTube Music',
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-      webSecurity: false
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false,
     },
   });
-  const userDataPath = path.join(app.getPath('userData'), 'music_youtube_data');
-  app.setPath('userData', userDataPath);
+
   win.loadURL('https://music.youtube.com');
+
+  win.webContents.setZoomFactor(1);
+
+
   const navigationMenu = Menu.buildFromTemplate([
     {
       label: '← Geri',
       click: () => {
-        if (win.webContents.canGoBack()) {
-          win.webContents.goBack();
-        }
+        if (win.webContents.canGoBack()) win.webContents.goBack();
       },
     },
     {
       label: 'İleri →',
       click: () => {
-        if (win.webContents.canGoForward()) {
-          win.webContents.goForward();
-        }
+        if (win.webContents.canGoForward()) win.webContents.goForward();
       },
     },
-  ],
 
-  );
+  ]);
   Menu.setApplicationMenu(navigationMenu);
 
-  win.webContents.setZoomFactor(1);
-
   win.on('closed', () => {
-    try {
-      win = null;
-    } catch (error) {
-      return
-    }
-
+    win = null;
   });
-
-
 }
 
 app.on('window-all-closed', () => {
   app.quit();
-  setTimeout(() => {
-    process.exit(0);
-  }, 500);
 });
 
 app.whenReady().then(createWindow);
